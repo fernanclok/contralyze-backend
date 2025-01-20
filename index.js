dca4fd3c495e7624180a78a4ea38c3767b5a33d5
Fernando Medina <fernanclok@gmail.com>
2025-01-20 14:57:46 -0800

    feat: created endpoint to users functions

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import express from "express";
-import { PORT } from "./config.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { sequelize } from "./connection.js";
 
 import userRoutes from "./routes/UserRoutes.js";
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+app.use (express.json());
+app.use(cookieParser());
 
 app.use("/user", userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+sequelize
+.authenticate()
+.then(() => {
+    console.log("Connection has been established successfully.");
+    return sequelize.sync();
+})
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("Unable to connect to the database:", error);
 });
---
+++ models/models.js
@@ -0,0 +1,48 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../connection.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  { tableName: "users" }
+);
+
+export { User };
